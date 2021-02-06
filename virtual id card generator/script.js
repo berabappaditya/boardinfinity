a09458378fd5e5d186247b0f7db0502e7132f716
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2021-02-06 14:53:34 +0530

    virtual id card complete update

--- virtual id card generator/script.js
+++ virtual id card generator/script.js
@@ -1,3 +1,26 @@
 function cardShow(){
   document.getElementById("idcard").style.display="flex";
+  nam=document.getElementById("stname").value;
+  clg=document.getElementById("college").value;
+  addrs=document.getElementById("adress").value;
+  document.getElementById("nameshow").innerHTML=`Name: ${nam}`;
+  document.getElementById("collegeshow").innerHTML=`College: ${clg}`;
+  document.getElementById("adressshow").innerHTML=`Adress: ${addrs}`;
 }
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+      $('#imgPrev').attr('src', e.target.result);
+      $('#idCardImg').attr('src', e.target.result);
+    }
+   
+    
+    reader.readAsDataURL(input.files[0]); // convert to base64 string
+  }
+}
+
+$("#imgInp").change(function() {
+  readURL(this);
+});
